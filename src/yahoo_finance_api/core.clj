ddfa183970f165a38df46be31a09f88fca9558ba
Matthew Siemens <contact@matthewsiemens.com>
2016-03-04 20:21:16 -0600

    Was still passing in string instead of keyword when we don't ask for specific fields

--- src/yahoo_finance_api/core.clj
+++ src/yahoo_finance_api/core.clj
@@ -3,7 +3,7 @@
 
 (defn get-stock [symbol & field-keys]
   (if (not field-keys)
-    (api/get-stock-from-api symbol (list "last-price"))
+    (api/get-stock-from-api symbol (list :last-price))
     (api/get-stock-from-api symbol field-keys)
     )
   )
