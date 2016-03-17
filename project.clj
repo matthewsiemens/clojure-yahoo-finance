0123616c1576c1428c34d44067cc3ee30ef2bdb1
Matthew Siemens <contact@matthewsiemens.com>
2016-03-16 20:00:54 -0600

    Update Clojure dependency to newest version * Increment version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject yahoo-finance-api "0.1.5"
+(defproject yahoo-finance-api "0.1.6"
   :description "An easy to use Clojure library for querying with the Yahoo Finance API"
   :url "https://github.com/matthewsiemens/clojure-yahoo-finance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-csv/clojure-csv "2.0.2"]])
