cf3cadf7aa0058da6c3920a74c28c19223ce5b15
aaelony <aaelony@gmail.com>
2017-05-14 20:58:11 -0700

    Adding a few more yahoo api param options

--- src/yahoo_finance_api/core.clj
+++ src/yahoo_finance_api/core.clj
@@ -4,6 +4,4 @@
 (defn get-stock [symbol & field-keys]
   (if (not field-keys)
     (api/get-stock-from-api symbol (list :last-price))
-    (api/get-stock-from-api symbol field-keys)
-    )
-  )
+    (api/get-stock-from-api symbol field-keys)))
